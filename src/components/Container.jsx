870dbe349e3d0643ef8c6eb29d704956799ba9a4
Pav0l <pavol.karas@gmail.com>
2019-04-09 10:48:25 +0200

    Fetch async data. Add new elements to the DOM

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './Container.less';
 
 export default class Container extends Component {
+  state = {
+    quotes: [],
+  };
+
+  getQuotes = () => {
+    axios.get('https://quotes.org/api').then(res => {
+      this.setState({
+        quotes: res.data,
+      });
+    });
+  };
+
   render() {
     return (
       <div className='container-hello-world'>
         Hello, World!
+        {this.state.quotes.map(q => (
+          <div key={q.id}>{q.text}</div>
+        ))}
+        <button onClick={this.getQuotes}>Get quotes</button>
       </div>
     );
   }
