01e30e792c7706f794e3d11fd2614a0e3286df44
Pav0l <pavol.karas@gmail.com>
2019-04-09 10:44:36 +0200

    Add first two tests. Add cleanup after tests

--- src/components/Container.test.jsx
+++ src/components/Container.test.jsx
@@ -1,3 +1,33 @@
 import React from 'react';
 import * as rtl from 'react-testing-library';
 import Container from './Container';
+
+// you want to clean up after each test, because changes in the DOM
+// to the component can persist between tests
+afterEach(rtl.cleanup);
+
+describe('Container', () => {
+  it('outputs "Hello World!" message', () => {
+    // RTL executes our component, so it spits out the DOM it produces
+    // we run assertions against that DOM that comes from the component
+    // but to render the component, we'll save it to a variable
+    const wrapper = rtl.render(<Container />);
+
+    // to visually see what the wrapper DOM render looks like you can use
+    // console.log(wrapper.debug());
+
+    // you dont need the second part of the assertion (.toBe(...))
+    // because if it would not have the string, the test would fail
+    // expect(wrapper.getByText('Hello, World!'));
+    // the above is quite brittle, its better to use RegExpressions
+    expect(wrapper.getByText(/world/i));
+  });
+
+  it('does not output the spinner "loading..."', () => {
+    const wrapper = rtl.render(<Container />);
+    // queryByText evaluates to falsy value if the string is not
+    // on the rendered component
+    // expect(wrapper.queryByText('loading...')).toBeFalsy();
+    expect(wrapper.queryByText(/loading/i)).toBeFalsy();
+  });
+});
